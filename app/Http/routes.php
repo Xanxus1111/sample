ceb6dafe711ab58cbbfa54fc07701410ccbcdf02
Xanxus1111 <809339558@qq.com>
2018-07-28 07:39:03 +0000

    Finish static pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,6 +10,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
